e6b31de4d566078d2471853fedd721671e76e9ea
Pityke <istvang16@gmail.com>
2026-01-25 19:11:36 +0100

    set new game

--- src/components/Tippmix.jsx
+++ src/components/Tippmix.jsx
@@ -1,12 +1,25 @@
-import {} from "react";
+import { useContext, useState } from "react";
 
 // bootstrap
 import { Col, Container, Row, Button } from "react-bootstrap";
 
 // context
-import {} from "../context/TippmixContext";
+import { TippmixContext } from "../context/TippmixContext";
 
 const Tippmix = () => {
+  const { randomNumber, min, max, tipp, addHidden, removeHidden } =
+    useContext(TippmixContext);
+  const [score, setScore] = useState();
+  const [firstScreen, setFirstScreen] = useState(removeHidden);
+  const [secoundScreen, setSecoundScreen] = useState(addHidden);
+  const [thirdScreen, setThirdScreen] = useState(addHidden);
+
+  const newGame = () => {
+    setFirstScreen(addHidden);
+    setSecoundScreen(removeHidden);
+    setThirdScreen(addHidden);
+    tipp();
+  };
   return (
     <>
       <Container className="bg-success text-center py-4">
@@ -17,30 +30,35 @@ const Tippmix = () => {
         </Row>
       </Container>
       <Container className="d-flex text-center py-3 bg-warning flex-column">
-        <Row className="align-content-center">
-          <Col className="" md="6">
+        <Row className="justify-content-center">
+          <Col className={`${firstScreen}`} md="6">
             <div className="rounded-5 p-4 mb-3 bg-info">
-              <h5>Gondoltam egy számra 0 és 1000 között</h5>
+              <h5>{`Gondoltam egy számra ${min} és ${max} között`}</h5>
             </div>
             <p className="opacity-50">Kitalálod?</p>
-            <Button className="bg-gradient rounded-5 py-2 px-4">Kezdés</Button>
+            <Button
+              className="bg-gradient rounded-5 py-2 px-4"
+              onClick={newGame}
+            >
+              Kezdés
+            </Button>
           </Col>
         </Row>
-        <Row className="align-content-center">
-          <Col className="visually-hidden" md="6">
+        <Row className="justify-content-center">
+          <Col className={`${secoundScreen}`} md="6">
             <div className="rounded-5 p-4 mb-3 bg-info">
               <h5>A szám a tippednél kisebb</h5>
             </div>
             <p className="opacity-50">lehetőség maradt</p>
             <h5>Mit tippelsz?</h5>
             <div className="mb-3">
-              <input type="text" />
+              <input type="text" onChange={(e) => setScore(e.target.value)} />
             </div>
             <Button className="bg-gradient rounded-5 py-2 px-4">Beküld</Button>
           </Col>
         </Row>
-        <Row className="align-content-center">
-          <Col className="visually-hidden" md="6">
+        <Row className="justify-content-center">
+          <Col className={`${thirdScreen}`} md="6">
             <div className="rounded-5 p-4 mb-3 bg-info">
               <h5>Vége a játéknak, nyertél!</h5>
             </div>
--- src/context/TippmixContextProvider.jsx
+++ src/context/TippmixContextProvider.jsx
@@ -1,10 +1,27 @@
-import {} from "react";
+import { useState } from "react";
 
 // context
 import { TippmixContext } from "./TippmixContext";
 
 export const TippmixContextProvider = ({ children }) => {
+  const [randomNumber, setRandomNumber] = useState();
+
+  const min = 0;
+  const max = 1000;
+
+  const tipp = () => {
+    const machineNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    setRandomNumber(machineNumber);
+    console.log(machineNumber);
+  };
+
+  const addHidden = "visually-hidden";
+  const removeHidden = "";
   return (
-    <TippmixContext.Provider value={{}}>{children}</TippmixContext.Provider>
+    <TippmixContext.Provider
+      value={{ randomNumber, min, max, tipp, addHidden, removeHidden }}
+    >
+      {children}
+    </TippmixContext.Provider>
   );
 };
